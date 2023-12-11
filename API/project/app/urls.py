8ad82336ddde81ca86924a2f682a4330e7002306
Liza Kapustina <liz.kapustina92@gmail.com>
2023-12-11 20:21:45 +0400

    Сhanged the url, added tests

--- API/project/app/urls.py
+++ API/project/app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('getData/', SubmitDataView.as_view(), name='submit_data'),
-    path('getSubmission/<int:id>/', GetSingleSubmissionView.as_view(), name='get_submission'),
-    path('patchSubmission/<int:id>/', PatchSubmissionView.as_view(), name='patch_submission'),
-    path('getUserSubmissions/', GetUserSubmissionsView.as_view(), name='get_user_submissions'),
+    path('get_data/', SubmitDataView.as_view(), name='submit_data'),
+    path('get_submitData/<int:id>/', GetSingleSubmissionView.as_view(), name='get_submission'),
+    path('patch_submitData/<int:id>/', PatchSubmissionView.as_view(), name='patch_submission'),
+    path('get_submitData/', GetUserSubmissionsView.as_view(), name='get_user_submissions'),
 ]
